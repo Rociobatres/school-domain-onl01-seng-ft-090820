fd935b544f3aa3869390250be268643d2d72f274
Rocio Batres <jonasgirl21@gmail.com>
2020-09-19 00:16:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+class School 
+  
+  attr_accessor  
+  attr_reader :name, :add_student, :roster, :grade, :sort     
+  
+  def initialize(name)
+    @name = name 
+  end   
+  
+  def roster
+    @roster = roster
+  end 
+  
+  hash = {}  
+    hash["new_key"] = []
+    hash["new_key"] << "new_value_for_value_array"
+ 
+  def add_student(name, grade)
+    @name = name 
+    @grade - grade
+  end  
+  
+  def grade(num)
+    @grade = grade ||= add_student
+  end 
+  
+  def sort
+    @sort
+  end   
+  
+  
+end   
